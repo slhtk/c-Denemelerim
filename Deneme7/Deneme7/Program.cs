c56ba53c0aa4dccc64e6d79eb5d677bdd7b9b2f3
slhtk <slhtk06@gmail.com>
2021-08-26 12:37:29 +0300

    deneme

---
+++ Deneme7/Deneme7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kullanıcı adınızı giriniz: ");
+            string kullaniciKodu = Console.ReadLine();
+            kullaniciKodu = kullaniciKodu.ToUpper(); // girilen değerler küçük harf olsa bile harfleri büyütüp üzerine yazar
+
+        // ToLower = harfleri küçültür.yukarıda ToLower kullansaydık girilen değer büyük olsaydı küçük harfe çevirip üzerine yazıcaktı. 
+
+            //SİSTEME GİRİŞİNE İZİN VERİLEN KULLANICI ADLARI
+            if (kullaniciKodu=="ABC" || kullaniciKodu=="123" || kullaniciKodu=="236" || kullaniciKodu=="CMK")
+            {
+                Console.Write("Kullanıcı girişi başarılı.");
+            }
+            // YASAKLI KULLANICILAR 
+            else if (kullaniciKodu=="HHH" || kullaniciKodu=="BBB" || kullaniciKodu=="MMM")
+            {
+                Console.Write("Hesabınız kilitlenmiştir.");
+            }
+            else
+            {
+                Console.Write("Hatalı kullanıcı kodu.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Deneme8/Deneme8/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - Toplama=: ");
+            Console.WriteLine("2 - Çıkartma: ");
+            Console.WriteLine("3 - Çarpma: ");
+            Console.WriteLine("4 - Bölme: ");
+            Console.Write("Seçiminizi yapınız: ");
+            string kullanıcıSecimi = Console.ReadLine();
+            decimal sayi1 = 0;
+            decimal sayi2 = 0;
+
+            if (kullanıcıSecimi == "1" || kullanıcıSecimi == "2" || kullanıcıSecimi == "3" || kullanıcıSecimi == "4")
+            {
+                Console.WriteLine("1. sayıyı giriniz: ");
+                string s1 = Console.ReadLine();
+                Console.WriteLine("2. sayıyı giriniz: ");
+                string s2 = Console.ReadLine();
+
+                sayi1 = Convert.ToDecimal(s1);
+                sayi2 = Convert.ToDecimal(s2);
+            }
+
+            if (kullanıcıSecimi == "1")
+            {
+                decimal toplam = sayi1 + sayi2;
+                Console.WriteLine("toplama sonucu = " + toplam);
+            }
+            else if (kullanıcıSecimi == "2")
+            {
+                decimal çıkart = sayi1 - sayi2;
+                Console.WriteLine("Çıkartma sonucu = " + çıkart);
+
+            }
+            else if (kullanıcıSecimi == "3")
+            {
+                decimal carpma = sayi1 * sayi2;
+                Console.WriteLine("carpma sonucu = " + carpma);
+
+            }
+            else if (kullanıcıSecimi == "4")
+            {
+                if (sayi2 == 0)
+                {
+                    Console.WriteLine("Bölen sayı 0 olamaz");
+                }
+                else
+                {
+                    decimal bolme = sayi1 / sayi2;
+                    Console.WriteLine("bölme sonucu = " + bolme);
+                }
+
+
+            }
+
+
+            else
+            {
+                Console.WriteLine("hatalı giriş yaptınız 1-4 arası bir seçim yapmalısınız ");
+
+            }
+            Console.WriteLine("Uygulama sonu");
+            Console.ReadLine();
+
+        }
+    }
+}
